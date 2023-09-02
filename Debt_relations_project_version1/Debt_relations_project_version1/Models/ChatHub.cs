b3facb035c900b9827f159e64b82c58c77dd95c0
dimakozlovich <62338799+dimakozlovich@users.noreply.github.com>
2023-09-02 22:33:42 +0300

    feat: add SignalR

---
+++ Debt_relations_project_version1/Debt_relations_project_version1/Models/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Debt_relations_project_version1.Models
+{
+	public class ChatHub : Hub
+	{
+		public async Task Send(string message, string connectionId)
+		{
+			await this.Clients.Client(connectionId).SendAsync(message);
+		}
+	}
+}
--- Debt_relations_project_version1/Debt_relations_project_version1/Program.cs
+++ Debt_relations_project_version1/Debt_relations_project_version1/Program.cs
@@ -1,10 +1,15 @@
+using Debt_relations_project_version1.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // устанавливаем сопоставление маршрутов с контроллерами
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Registration}/{action=Entrance}/{id?}");
+
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
